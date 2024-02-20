f5124a9eeae733392d7452a23ccc0ff48273dbb4
Qudus <qudus@mit.edu>
2024-02-20 02:41:52 -0500

    v1 release

--- app/discover/photographer-results.tsx
+++ app/discover/photographer-results.tsx
@@ -13,7 +13,7 @@ export default async function PhotographerResults({
 	className,
 }: PhotographerResultsProps) {
 	const photographers = await getPhotographers()
-	// shuffleArray(photographers)
+	shuffleArray(photographers)
 
 	if (!photographers || !Array.isArray(photographers)) {
 		return <div className={cn('space-y-5', className)}>No photographers found.</div>
