8cb2dcc8275906b51f5d692febd6d0dcb31272e7
Qudus <95336060+qudush2@users.noreply.github.com>
2024-04-08 20:23:53 -0400

    remove messages from nav

--- app/navigation-bar.tsx
+++ app/navigation-bar.tsx
@@ -37,7 +37,7 @@ export default function NavigationBar() {
             Discover
           </NavigationLink>
         </NavbarItem>
-        {userId && (
+        {/* {userId && (
           <NavbarItem>
             <NavigationLink
               href={`/messages`}
@@ -47,7 +47,7 @@ export default function NavigationBar() {
               Messages
             </NavigationLink>
           </NavbarItem>
-        )}
+        )} */}
       </NavbarContent>
 
       <NavbarContent justify="end" className="gap-4">
@@ -60,6 +60,7 @@ export default function NavigationBar() {
             Discover
           </NavigationLink>
         </NavbarItem>
+        {/* {userId && (
         <NavbarItem className="flex md:hidden">
           <NavigationLink
             href={`/messages`}
@@ -69,6 +70,7 @@ export default function NavigationBar() {
             Messages
           </NavigationLink>
         </NavbarItem>
+          )} */}
         <NavbarItem>
           {userId ? (
             <UserButton
