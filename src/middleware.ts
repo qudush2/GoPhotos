c7707e5690ea91843a3cc18014bae817a9a9a5bb
Qudus <95336060+qudush2@users.noreply.github.com>
2024-08-01 18:48:43 -0400

    update clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,18 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-type SessionClaims = {
-  metadata?: {
-    admin?: boolean;
-  };
-};
-
-// if gophotos.us/qudus --> change the public route to include /.*
 const publicRoutes = createRouteMatcher([
   '/',
   '(^/discover.*)',
-  '/api/webhooks/(.*)'
-]);
-
-const adminRoutes = createRouteMatcher([
-  '/admin(.*)',
+  '/api/webhooks/(.*)',
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (adminRoutes(req)) {
-    // Protect admin routes and check for admin metadata
-    auth().protect();
-    const { sessionClaims } = auth();
-    if (!(sessionClaims as SessionClaims)?.metadata?.admin) {
-      return new Response("Access denied", { status: 403 });
-    }
-  } else if (!publicRoutes(req)) {
-    // Protect all other routes except public and ignored ones
+  if (!publicRoutes(req)) {
+    // Protect all routes except the public ones
     auth().protect();
   }
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+ matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
