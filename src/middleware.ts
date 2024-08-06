d038890bc70fbd183046ca4536e3ade7e1e30111
Qudus <95336060+qudush2@users.noreply.github.com>
2024-08-06 17:46:51 -0400

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,11 +26,16 @@ export default clerkMiddleware((auth, req) => {
       return new Response("Access denied", { status: 403 });
     }
   } else if (!publicRoutes(req)) {
-    // Protect all other routes except public and ignored ones
+    // Protect all other routes except public
     auth().protect();
   }
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
