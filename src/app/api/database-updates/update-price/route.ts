8fdf5c3c976fa8ea54cc600f440c4c35cde60984
Qudus <95336060+qudush2@users.noreply.github.com>
2024-11-23 18:28:13 -0500

    update message panel to reflect mit_po

--- src/app/api/database-updates/update-price/route.ts
+++ src/app/api/database-updates/update-price/route.ts
@@ -1,12 +1,21 @@
 import { NextRequest } from "next/server";
-import { updateJobPrice } from "@/src/utils/db";
+import { updateJobPrice, updatePaid, getJobDetails } from "@/src/utils/db";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const convoID = body.convoID as string;
   const job_price = body.job_price as string;
 
+  const jobDetails = await getJobDetails(convoID);
+
   await updateJobPrice(convoID, job_price);
+  if (jobDetails.mit_po) {
+    await updatePaid(
+      convoID,
+      jobDetails.customer_clerk_id,
+      jobDetails.photographer_clerk_id
+    );
+  }
 
   if (req.method === "POST") {
     return new Response("price updated", { status: 200 });
